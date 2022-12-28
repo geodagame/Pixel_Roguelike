ecd750561e2c3eab32b667e033dc6d515fa38087
IzumiKei <izumikeiper@gmail.com>
2022-12-28 18:58:36 -0300

    Notes in code, floor tileset

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -3,20 +3,23 @@ extends StaticBody2D
 
 onready var animationPlayer = $AnimationPlayer
 
-var state = "close"
+var state = "close" #Para evitar que se abra la puerta cuando ya lo está
 
 func _ready():
 	open()
 
 func open():
+	#Abre la puerta
 	if state == "close":
 		animationPlayer.play("Open");
 		state = "open"
 
 func close():
+	#Cierra la puerta
 	if state == "open":
 		animationPlayer.play("Close");
 		state = "close"
 
 func _get_state():
+	#En caso de que algo necesite saber el estado de una puerta
 	return state
--- scripts/RoomScript.gd
+++ scripts/RoomScript.gd
@@ -8,23 +8,34 @@ onready var _container_enemyPosition = $EnemyPosition
 onready var playerDetector = $PlayerDetector
 
 func _ready():
+	# Cuenta la cantidad de Position2D en EnemyPosition
+	
 	num_enemies = _container_enemyPosition.get_child_count();
 	
 func _on_enemy_killed():
+	# Lleva registro de la cantidad de enemigos vivos.
+	
 	num_enemies -= 1
 	if num_enemies <= 0:
+		# Si no hay más enemigos, abre la puerta.
+		
 		for door in _container_doors.get_children():
 			door.open()
 
 func _spawn_monsters():
+	# Coloca un enemigo en cada Position2D
+	
 	for enemyPosition in _container_enemyPosition.get_children():
 		var enemy = enemy_data["Test Monster"].instance()
+		
 		enemy.connect("tree_exited", self, "on_enemy_killed")
 		enemy.global_position = enemyPosition.global_position
 		call_deferred("add_child", enemy)
 		
 
 func _initialize_room():
+	# Inicializa la habitación.
+	
 	playerDetector.queue_free()
 	_spawn_monsters()
 	for door in _container_doors.get_children():
