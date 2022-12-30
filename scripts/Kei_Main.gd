1460940ddb13a29bbdeefed9956ff26b1093ca63
IzumiKei <izumikeiper@gmail.com>
2022-12-30 17:58:44 -0300

    New Door Sprite, new RoomWalls Sprite

--- scripts/Kei_Main.gd
+++ scripts/Kei_Main.gd
@@ -8,8 +8,20 @@ func _ready():
 	pass
 
 func _process(_delta):
+	
+	#---------------
+	#DevOnly Section
+	#---------------
 	if Input.is_action_just_pressed("ui_accept"):
 		#DevOnly: Inicia el juego
 		start_room._initialize_room();
+	if Input.is_action_just_pressed("ui_down"):
+		#DevOnly: Fuerza la apertura de todas las puertas
+		for door in start_room._container_doors.get_children():
+			door.open()
+	if Input.is_action_just_pressed("ui_up"):
+		#DevOnly: Fuerza el cierre de todas las puertas
+		for door in start_room._container_doors.get_children():
+			door.close()
 		
 	
